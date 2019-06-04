b0365be87ce361e2e9f1779c9b3b99fdc018a38d
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-04 12:47:58 +0200

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 12:31:47 by ssithole          #+#    #+#             */
-/*   Updated: 2019/06/03 12:38:40 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/04 12:47:01 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,12 @@ int		ft_atoi(char *str)
 	sign = 1;
 	if (str == '\0')
 		return (0);
-	while (ft_isspace(str[i])
-        {
+	while (ft_isspace(str[i]))
             i += 1;
-        }
 	if (str[i] == '-' || str[i] == '+')
-    {
 		if (str[i++] == '-')
-        {
 			sign = -1;
-        }
-    }
 	while (str[i] >= '0' && str[i] <= '9')
-    {
 		nbr = (nbr * 10) + (str[i++] - '0');
-	}
 	return (nbr * sign);
 }
