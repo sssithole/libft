ba3f95258cf741f2da1b3320af5299e13742a087
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-06 12:49:19 +0200

    libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 09:25:04 by ssithole          #+#    #+#             */
-/*   Updated: 2019/05/30 12:14:38 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/06 12:48:29 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,27 @@ int				ft_isdigit(int c);
 int				ft_tolower(int c);
 int				ft_isprint(int c);
 int				ft_isascii(int c);
+int             ft_isprint(int c);
+int             ft_isspace(int i);
+int             ft_atoi(char *str);
+size_t          ft_strlen(const char *s);
+char            *ft_strncat(char *s1, const char *s2, size_t n);
+char            *ft_strchr(const char *s, int c);
+char            *ft_strrchr(const char *s, int c);
+char            *ft_strncpy(char *dst, const char *src, size_t len);
+
+//2nd pat
+
+void            ft_putchar(char c);
+void            ft_putstr(char const *s);
+void            ft_putendl(char const *s);
+void            ft_putchar_fd(char c, int fd);
+void            ft_putstr_fd(char const *s, int fd);
+void            ft_putendl_fd(char const *s, int fd);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
+char			*ft_strnew(size_t size);
+void			ft_strdel(char **as);
+void			ft_strclr(char *s);
 
 #endif
