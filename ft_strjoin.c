27b6724e02fa770c479245edf6b98d15fb88a5bc
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-10 13:19:54 +0200

    strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 12:45:01 by ssithole          #+#    #+#             */
+/*   Updated: 2019/06/10 13:18:16 by ssithole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t l1;
+	size_t l2;
+	char *str;
+
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(str) * (l1 +l2) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+
+	return (str);
+}
