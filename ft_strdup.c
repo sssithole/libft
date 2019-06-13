6a2aeaef135d05ba94f6886793a31a23f088a30f
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-13 15:42:28 +0200

    ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,26 +6,28 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:51:45 by ssithole          #+#    #+#             */
-/*   Updated: 2019/05/31 09:57:50 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:39:32 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char		*ft_strdup(const char *s)
 {
-	char	*copy;
+	char	*dest;
+	size_t	len;
 	int		i;
 
-	i =-1;
-	if (!s1 || !(copy = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1)))
-	{
+	i = 0;
+	len = ft_strlen(s);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
 		return (NULL);
-	}
-	while (s1[++i])
+	while (s[i] != '\0')
 	{
-		copy[i] = s1[i];
+		dest[i] = s[i];
+		i++;
 	}
-	copy[i] = '\0';
-	return (copy);
+	dest[i] = '\0';
+	return (dest);
 }
