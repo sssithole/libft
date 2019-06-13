09d5bbf5ad2091dc51222c6dc45d6002a0e554cf
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-13 15:41:39 +0200

    ft_strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,27 +6,27 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 11:34:47 by ssithole          #+#    #+#             */
-/*   Updated: 2019/06/05 13:36:05 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:37:13 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strncpy(char *dst, const char *src, size_t len)
+char	ft_strncpy(char *dst, const char *src, size_t len)
 {
-    int     i;
+	int i;
 
-    i = 0;
-    while (src != '\0' && (i < len))
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    while (i < len)
-    {
-        dst[i] = '\0';
-        i++;
-    }
-    dst[i] = '\0';
-    return (dst);
+	i = 0;
+	while (src != '\0' && (i < len))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < len)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	dst[i] = '\0';
+	return (*dst);
 }
