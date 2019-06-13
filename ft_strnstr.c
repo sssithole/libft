182beec72d06a607892fb7d1ffc30e431a9ed565
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-13 16:15:19 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,26 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 14:44:22 by ssithole          #+#    #+#             */
-/*   Updated: 2019/06/11 15:16:13 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/13 16:14:12 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strnstr(const char *haystack, const char *needle,size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    size_t l;
-    size_t m;
-    
-    l = 0;
-    if(needle[0] == '\0')
-    {
-        return((char *)haystack);
-    }
-    while(haystack[l] && (l + strlen(needle) <= len))
-    {
-        m = 0;
-        while(haystack[l + m] == needle[m])
-        {
-            if(m == strlen(needle) - 1)
-            {
-                return((char *)haystack);
-            }
-            m++;
-        }
-        l++;
-    }
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < n && haystack[i] != '\0')
+	{
+		j = 0;
+		while (i + j < n && haystack[i + j] == needle[j] && needle[j] != '\0')
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
 	return (0);
-} 
+}
