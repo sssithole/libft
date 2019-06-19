707c6bd423b570ede13e64f8758acadada2caf39
Sikhumbuzo SITHOLE <ssithole@c4r4s3.wethinkcode.co.za>
2019-06-19 14:33:43 +0200

    it here

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,33 +6,43 @@
 /*   By: ssithole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 10:42:17 by ssithole          #+#    #+#             */
-/*   Updated: 2019/06/18 15:13:02 by ssithole         ###   ########.fr       */
+/*   Updated: 2019/06/19 14:31:49 by ssithole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static	size_t	ft_check_neg(int n)
 {
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
+char			*ft_itoa(int nbr)
+{
+	int		temp;
+	int		len;
+	int		neg;
 	char	*str;
 
-	str = (char *)malloc(sizeof(char) * 2);
+	temp = nbr;
+	len = 2;
+	neg = (nbr < 0) ? 1 : 0;
+	while ((temp = temp / 10))
+		len++;
+	len = len + neg;
+	str = (char *)malloc(sizeof(char) * len);
 	if (str == NULL)
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(str, "-2147483648"));
-	if (n < 0)
+	str[--len] = '\0';
+	while (len--)
 	{
+		str[len] = ft_check_neg(nbr % 10) + '0';
+		nbr = nbr / 10;
+	}
+	if (neg == 1)
 		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
-	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
 	return (str);
 }
